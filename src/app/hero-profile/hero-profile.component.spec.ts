85fede9f3dad5661d21b82a46b55c7df141ac19a
matias aguilera <matias.aguilera@siigroup.cl>
2021-11-03 14:34:59 -0300

    unit test hero profile y lista heroe

--- src/app/hero-profile/hero-profile.component.spec.ts
+++ src/app/hero-profile/hero-profile.component.spec.ts
@@ -4,10 +4,25 @@ import { HeroesService } from '../heroes.service';
 
 import { HeroProfileComponent } from './hero-profile.component';
 
+let heroe = [{
+  id:'1',
+  name:'Spiderman1',
+  description: 'El hombre que araña',
+  modified:new Date(1518417160),
+  thumbnail:
+  {
+  'path': 'https://i.pinimg.com/originals/c2/93/56/c293563aa553250601d8cb768c044d4b',
+  'extension': 'jpg'
+  },
+  resourceURI:'http://gateway.marvel.com/v1/public/characters/1011334',
+  teamColor:'yellow'
+}]
+
 describe('HeroProfileComponent', () => {
   let component: HeroProfileComponent;
   let fixture: ComponentFixture<HeroProfileComponent>;
   let heroeService: HeroesService;
+  let mock = heroe
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       
@@ -17,14 +32,33 @@ describe('HeroProfileComponent', () => {
     .compileComponents();
   }));
 
+  
+
   beforeEach(() => {
     fixture = TestBed.createComponent(HeroProfileComponent);
-    heroeService = TestBed.get(HeroesService);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    heroeService = TestBed.get(HeroesService);
+    
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(component).toBeDefined();
+  });
+
+  it('should goBack', () => {
+    spyOn(component,'goBack').and.callThrough();
+    component.goBack();
+    expect(component.goBack).toHaveBeenCalled();
+  });
+
+  it('should getTeam', () => {
+    spyOn(component,'getTeam').and.callThrough();
+    component.getTeam(mock[0].teamColor);
+    expect(component.goBack).toHaveBeenCalled();
+  });
+  it('should buscarHero', () => {
+    spyOn(component,'buscarHero').and.callThrough();
+    component.buscarHero();
+    expect(component.buscarHero).toHaveBeenCalled();
   });
 });
--- src/app/listado-de-heroes/listado-de-heroes.component.spec.ts
+++ src/app/listado-de-heroes/listado-de-heroes.component.spec.ts
@@ -1,60 +1,81 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, ComponentFixtureAutoDetect, inject, TestBed } from '@angular/core/testing';
 import { of } from 'rxjs';
 import { Heroe } from '../classes/heroe';
 import { HeroesService } from '../heroes.service';
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { ListadoDeHeroesComponent } from './listado-de-heroes.component';
+import { AppComponent } from '../app.component';
+import { ModalPollComponent } from '../modal-poll/modal-poll.component';
+import { HeroProfileComponent } from '../hero-profile/hero-profile.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Store } from '@ngrx/store';
+import { createComponent } from '@angular/compiler/src/core';
+import { exec } from 'child_process';
+
+let heroe = [{
+  id:'1',
+  name:'Spiderman1',
+  description: 'El hombre que araña',
+  modified:new Date(1518417160),
+  thumbnail:
+  {
+  'path': 'https://i.pinimg.com/originals/c2/93/56/c293563aa553250601d8cb768c044d4b',
+  'extension': 'jpg'
+  },
+  resourceURI:'http://gateway.marvel.com/v1/public/characters/1011334',
+  teamColor:'yellow'
+}]
 
 describe('ListadoDeHeroesComponent', () => {
   let component: ListadoDeHeroesComponent;
   let fixture: ComponentFixture<ListadoDeHeroesComponent>;
-
-  let httpClientSpy: {get: jasmine.Spy}
+  let mock = heroe;
   let heroeService: HeroesService;
-  beforeEach(async(() => {
-    httpClientSpy = jasmine.createSpyObj('HttpClient',['get']);
-    heroeService = new HeroesService(httpClientSpy as any);
+ 
+  beforeEach(()=>{
     TestBed.configureTestingModule({
-      declarations: [ ListadoDeHeroesComponent ]
-    })
-    .compileComponents();
-   
-  }));
-
-  beforeEach(() => {
-    
-    // fixture = TestBed.createComponent(ListadoDeHeroesComponent);
-    // component = fixture.componentInstance;
-    // fixture.detectChanges();
-    httpClientSpy = jasmine.createSpyObj('HttpClient',['get']);
-    heroeService = new HeroesService(httpClientSpy as any);
-  });
+      declarations:[ListadoDeHeroesComponent],
+      providers:[HeroesService]
+    }).compileComponents();
+  })
+ 
+  beforeEach(()=>{
+    fixture = TestBed.createComponent(ListadoDeHeroesComponent);
+    component= fixture.componentInstance;
+    heroeService = TestBed.get(HeroesService);
+  })
 
   it('should create', () => {
-    expect(heroeService).toBeTruthy();
+    expect(component).toBeDefined();
   });
 
-  // it('deberia retornar objeto heroe lista',(done:DoneFn)=>{
+  it('should sumbmit serach',()=>{
+    spyOn(component,'submitSearch').and.callThrough();
+    component.submitSearch();
+    expect(component.submitSearch).toHaveBeenCalled();
 
-  //   let heroe = [{
-  //     id:'1',
-  //     name:'Spiderman1',
-  //     description: 'El hombre que araña',
-  //     modified:new Date(1518417160),
-  //     thumbnail:
-  //     {
-  //     'path': 'https://i.pinimg.com/originals/c2/93/56/c293563aa553250601d8cb768c044d4b',
-  //     'extension': 'jpg'
-  //     },
-  //     resourceURI:'http://gateway.marvel.com/v1/public/characters/1011334',
-  //     teamColor:'yellow'
-  //   }]
+  });
+  it('should go to id',() => {
+    spyOn(component,'go_to').and.callThrough();
+    component.go_to(heroe[0].id);
+    expect(component.go_to).toHaveBeenCalled();
+  })
+  it('should listar heores',()=>{
+    spyOn(component,'listarHeroes').and.callThrough();
+    component.listarHeroes();
+    expect(component.listarHeroes).toHaveBeenCalled();
+  })
+  
+  it('should next page',()=>{
+    spyOn(component,'nextPage').and.callThrough();
+    component.nextPage();
+    expect(component.nextPage).toHaveBeenCalled();
+  })
 
-  //   httpClientSpy.get.and.returnValue(of(heroe));
-  //   heroeService.listarHeroes().subscribe(
-  //     resultado=>{
-        
-  //     }
-  //   );
-  // })
+  it('should prev page',()=>{
+    spyOn(component,'prevPage').and.callThrough();
+    component.prevPage();
+    expect(component.prevPage).toHaveBeenCalled();
+  })
 });
